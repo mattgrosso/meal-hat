13fd52d686876ed62e5320996ac0434340d86413
Matt Grosso <matt.grosso@framebridge.com>
2024-07-07 10:56:12 -0400

    Switches to firebase auth

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
-import vue3GoogleLogin from 'vue3-google-login';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import VCalendar from 'v-calendar';
@@ -12,10 +11,6 @@ import './registerServiceWorker'
 const app = createApp(App);
 
 app.use(store);
-
-app.use(vue3GoogleLogin, {
-  clientId: '871807065045-mo4955ma7sf3etvkrqbrqn6j1vmkghca.apps.googleusercontent.com'
-});
 
 app.use(router);
 
